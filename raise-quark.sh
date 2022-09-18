949ad0e4403fc5d029c151258de185641154b011
Vincent Alexander Saulys <vincent@saulys.me>
2022-09-18 16:24:25 -0400

    add quark build script

---
+++ raise-quark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker build . -t personal-page-quark
+
+echo "### starting quark"
+docker run -d \
+       --net my-nginx-proxy-network \
+       --expose 8081 \
+       -e VIRTUAL_HOST=vincentsaulys.com \
+       -e LETSENCRYPT_HOST=vincentsaulys.com \
+       -e VIRTUAL_PORT=8081 \
+       -e LETSENCRYPT_EMAIL=vincent@saulys.me \
+       --name personal-page-quark \
+       personal-page-quark

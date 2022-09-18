505b5db65fb09c9bb9a9016a36210043a969daa8
Vincent Alexander Saulys <vincent@saulys.me>
2022-09-18 17:09:29 -0400

    updated page

--- raise-quark.sh
+++ raise-quark.sh
@@ -6,6 +6,7 @@ echo "### starting quark"
 docker run -d \
        --net my-nginx-proxy-network \
        --expose 8081 \
+       -e PORT=8081 \
        -e VIRTUAL_HOST=vincentsaulys.com \
        -e LETSENCRYPT_HOST=vincentsaulys.com \
        -e VIRTUAL_PORT=8081 \
